079d826f311b5445f1849e48701e76fba8180b75
Anastasia Dobryanskaya <180897_nastya@mail.ru>
2017-05-27 18:11:11 +0300

    Немного ботов

--- answers.py
+++ answers.py
@@ -2,5 +2,5 @@ def get_answer(question):
 	question = question.lower()
 	answers = {'hi': 'Hello', 'How are you?':'I am fine','Goodbye':'Bye'}
 	a = answers.get(question)
-	return a#.lower()
+	return a
 print(get_answer('HI'))
